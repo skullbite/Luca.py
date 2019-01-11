8ae0e00ac6fbc1b689f3471d93cfe4cc96380e80
Skullbite <mawler12@gmail.com>
2019-01-11 08:47:25 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@
 import discord
 from discord.ext import commands
 
-
+import random
 
 import requests as requests
 bot = commands.Bot(prefix="!")
@@ -84,6 +84,29 @@ async def on_message(message):
     elif message.content == '!help':
         return await message.author.send('Commands: `!help`, `!avatar`, `!about`, `!thonk`, `!cat`, `!ping`, `!8ball`, `!roll`, `!eval`, `!say`, `!randomcat`')
         
-
+@bot.event
+async def on_message(message):
+    if ''.join(message.content.split('')) == '!8ball':
+        replies = ["It is certain",
+		"It is decidedly so",
+			"Without a doubt",
+		"Yes, definitely",
+			"You may rely on it",
+		"As I see it, yes",
+			"Most likely",
+		"Outlook good",
+			"Yes",
+		"Signs point to yes",
+			"Reply hazy try again",
+		"Ask again later",
+			"Better not tell you now",
+		"Cannot predict now",
+			"Concentrate and ask again",
+		"Don't count on it",
+			"My reply is no",
+		"My sources say no",
+			"Outlook not so good",
+"Very doubtful"]
+        await message.channel.send(message.author.mention + ', ' + random.choice(replies))
         
 
